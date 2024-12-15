3b460e9a6e5d09db96c70abb683fdca0b190678b
andresjose098 <andres-jose1998@hotmail.com>
2024-12-15 14:23:15 -0500

    commit

--- tienda.sql
+++ tienda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-12-2024 a las 18:38:46
+-- Tiempo de generación: 15-12-2024 a las 20:22:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -38,7 +38,10 @@ CREATE TABLE `adiciones` (
 --
 
 INSERT INTO `adiciones` (`id`, `producto_id`, `nombre_adicion`) VALUES
-(25, 44, '');
+(25, 44, ''),
+(26, 45, 'Arequipe'),
+(27, 46, 'Lechera'),
+(28, 47, 'Lechera');
 
 -- --------------------------------------------------------
 
@@ -53,6 +56,7 @@ CREATE TABLE `clientes` (
   `direccion` varchar(255) NOT NULL,
   `telefono` varchar(20) NOT NULL,
   `producto` varchar(100) NOT NULL,
+  `cantidad` varchar(100) NOT NULL,
   `metodopago` varchar(100) NOT NULL,
   `domicilio` varchar(100) NOT NULL,
   `valortotal` varchar(100) NOT NULL,
@@ -63,8 +67,11 @@ CREATE TABLE `clientes` (
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `nombre`, `barrio`, `direccion`, `telefono`, `producto`, `metodopago`, `domicilio`, `valortotal`, `cambio`) VALUES
-(44, 'jorge alban', 'dasda', 'carrera 18 sur #8-16', '3185212067', '', 'Nequi', '$4.000', '150000', '200000');
+INSERT INTO `clientes` (`id`, `nombre`, `barrio`, `direccion`, `telefono`, `producto`, `cantidad`, `metodopago`, `domicilio`, `valortotal`, `cambio`) VALUES
+(44, 'jorge alban', 'dasda', 'carrera 18 sur #8-16', '3185212067', '', '', 'Nequi', '$4.000', '150000', '200000'),
+(46, 'jorge alban', 'dasda', 'carrera 18 sur #8-16', '3185212067', '', 'Merengon mixto', 'Nequi', '', '150000', '200000'),
+(47, 'jorge alban', 'dasda', 'carrera 18 sur #8-16', '3185212067', '', 'Merengon mixto', 'Nequi', '$14.000', '150000', '200000'),
+(48, 'jorge alban', 'dasda', 'carrera 18 sur #8-16', '3185212067', '', '', 'Bancolombia', '$8.000', '150000', '200000');
 
 -- --------------------------------------------------------
 
@@ -83,7 +90,10 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `usuario_id`, `nombre_producto`) VALUES
-(44, 44, 'Merengon mixto');
+(44, 44, 'Merengon mixto'),
+(45, 46, 'Merengon mixto'),
+(46, 46, 'Merengon Guanabana'),
+(47, 47, 'Merengon mixto');
 
 --
 -- Índices para tablas volcadas
@@ -117,19 +127,19 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `adiciones`
 --
 ALTER TABLE `adiciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- Restricciones para tablas volcadas
